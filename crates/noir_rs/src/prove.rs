043d76def726c5b3b15b3a898c2fc00ebce2b9df
Theo Madzou <madztheo@hotmail.fr>
2024-07-11 11:01:20 +0200

    add experimental support of honk proofs

--- crates/noir_rs/src/prove.rs
+++ crates/noir_rs/src/prove.rs
@@ -4,8 +4,7 @@ use acir::{circuit::{self, Circuit, Program}, native_types::{WitnessMap, Witness
 use base64::{engine::general_purpose, Engine};
 use bb_rs::barretenberg_api::{
     acir::{
-        acir_create_proof, acir_get_verification_key, acir_init_proving_key, delete_acir_composer,
-        get_circuit_sizes, new_acir_composer, CircuitSizes, acir_create_circuit
+        acir_create_circuit, acir_create_proof, acir_get_honk_verification_key, acir_get_verification_key, acir_init_proving_key, acir_prove_ultra_honk, delete_acir_composer, get_circuit_sizes, new_acir_composer, CircuitSizes
     },
     srs::init_srs,
 };
@@ -49,7 +48,7 @@ pub fn prove(
     Ok(unsafe {
         init_srs(&srs.g1_data, srs.num_points, &srs.g2_data);
         let mut acir_ptr = new_acir_composer(subgroup_size);
-        acir_init_proving_key(&mut acir_ptr, &acir_buffer_uncompressed);
+        //acir_init_proving_key(&mut acir_ptr, &acir_buffer_uncompressed);
         let result = (
             acir_create_proof(
                 &mut acir_ptr,
@@ -59,6 +58,50 @@ pub fn prove(
             acir_get_verification_key(&mut acir_ptr),
         );
         delete_acir_composer(acir_ptr);
+        result
+    })
+}
+
+
+pub fn prove_honk(
+    circuit_bytecode: String,
+    initial_witness: WitnessMap<FieldElement>,
+) -> Result<(Vec<u8>, Vec<u8>), String> {
+    let acir_buffer = general_purpose::STANDARD
+        .decode(circuit_bytecode)
+        .map_err(|e| e.to_string())?;
+    
+    let program = Program::deserialize_program(&acir_buffer).map_err(|e| e.to_string())?;
+
+    let mut decoder = GzDecoder::new(acir_buffer.as_slice());
+    let mut acir_buffer_uncompressed = Vec::<u8>::new();
+    decoder
+        .read_to_end(&mut acir_buffer_uncompressed)
+        .map_err(|e| e.to_string())?;
+
+    let blackbox_solver = Bn254BlackBoxSolver::default();
+
+    let solved_witness =
+        execute_circuit(&program, initial_witness, &blackbox_solver).map_err(|e| e.to_string())?;
+    let witness_stack = WitnessStack::try_from(solved_witness).map_err(|e| e.to_string())?;
+    let serialized_solved_witness =
+        bincode::serialize(&witness_stack).map_err(|e| e.to_string())?;
+        
+    let circuit_size = unsafe { get_circuit_sizes(&acir_buffer_uncompressed) };
+    let log_value = (circuit_size.total as f64).log2().ceil() as u32;
+    let subgroup_size = 2u32.pow(log_value);
+
+    let srs = NetSrs::new(subgroup_size + 1);
+
+    Ok(unsafe {
+        init_srs(&srs.g1_data, srs.num_points, &srs.g2_data);
+        let result = (
+            acir_prove_ultra_honk(
+                &acir_buffer_uncompressed,
+                &serialized_solved_witness,
+            ),
+            acir_get_honk_verification_key(&acir_buffer_uncompressed),
+        );
         result
     })
 }
--- crates/noir_rs/src/verify.rs
+++ crates/noir_rs/src/verify.rs
@@ -3,8 +3,7 @@ use std::io::Read;
 use base64::{engine::general_purpose, Engine};
 use bb_rs::barretenberg_api::{
     acir::{
-        acir_load_verification_key, acir_verify_proof, delete_acir_composer, get_circuit_sizes,
-        new_acir_composer,
+        acir_load_verification_key, acir_verify_proof, acir_verify_ultra_honk, delete_acir_composer, get_circuit_sizes, new_acir_composer
     },
     srs::init_srs,
 };
@@ -39,6 +38,34 @@ pub fn verify(
         acir_load_verification_key(&mut acir_ptr, &verification_key);
         let result = acir_verify_proof(&mut acir_ptr, &proof);
         delete_acir_composer(acir_ptr);
+        result
+    })
+}
+
+pub fn verify_honk(
+    circuit_bytecode: String,
+    proof: Vec<u8>,
+    verification_key: Vec<u8>,
+) -> Result<bool, String> {
+    let acir_buffer = general_purpose::STANDARD
+        .decode(circuit_bytecode)
+        .map_err(|e| e.to_string())?;
+
+    let mut decoder = GzDecoder::new(acir_buffer.as_slice());
+    let mut acir_buffer_uncompressed = Vec::<u8>::new();
+    decoder
+        .read_to_end(&mut acir_buffer_uncompressed)
+        .map_err(|e| e.to_string())?;
+
+    let circuit_size = unsafe { get_circuit_sizes(&acir_buffer_uncompressed) };
+    let log_value = (circuit_size.total as f64).log2().ceil() as u32;
+    let subgroup_size = 2u32.pow(log_value);
+
+    let srs = NetSrs::new(subgroup_size + 1);
+
+    Ok(unsafe {
+        init_srs(&srs.g1_data, srs.num_points, &srs.g2_data);
+        let result = acir_verify_ultra_honk( &proof, &verification_key);
         result
     })
 }
