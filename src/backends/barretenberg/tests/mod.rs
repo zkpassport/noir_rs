10c95ae31c192e50c4fc3fb4508298c67470c2b9
Theo Madzou <madztheo@hotmail.fr>
2025-07-11 17:46:29 +0100

    fix tests

--- src/backends/barretenberg/tests/mod.rs
+++ src/backends/barretenberg/tests/mod.rs
@@ -1,6 +1,6 @@
 use tracing::info;
-use bb_rs::barretenberg_api::{acir::{get_circuit_sizes, acir_vk_as_fields_ultra_honk}, srs::init_srs};
-use crate::backends::barretenberg::{srs::{setup_srs_from_bytecode, setup_srs, netsrs::NetSrs}, verify::{verify_ultra_honk, verify_ultra_keccak_honk, get_ultra_honk_verification_key, get_ultra_honk_keccak_verification_key}, prove::{prove_ultra_honk, prove_ultra_honk_keccak}, recursion, utils::compute_subgroup_size};
+use bb_rs::barretenberg_api::{acir::get_circuit_sizes, srs::init_srs};
+use crate::backends::barretenberg::{srs::{setup_srs_from_bytecode, setup_srs, netsrs::NetSrs}, verify::{verify_ultra_honk, verify_ultra_honk_keccak, get_ultra_honk_verification_key, get_ultra_honk_keccak_verification_key}, prove::{prove_ultra_honk, prove_ultra_honk_keccak}, utils::compute_subgroup_size};
 use acir::{FieldElement, native_types::{Witness, WitnessMap}};
 use crate::{witness, circuit};
 use serde_json;
@@ -67,7 +67,7 @@ fn test_ultra_honk_keccak() {
     let proof = prove_ultra_honk_keccak(keccak_circuit_bytecode, initial_witness, vk.clone(), false).unwrap();
     info!("ultra honk proof generation time: {:?}", start.elapsed());
 
-    let verdict = verify_ultra_keccak_honk(proof, vk, false).unwrap();
+    let verdict = verify_ultra_honk_keccak(proof, vk, false).unwrap();
     info!("honk proof verification verdict: {}", verdict);
 }
 
